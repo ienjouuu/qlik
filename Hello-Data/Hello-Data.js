8fb133ed2831f91cf307547f5946061d674fdce0
Сергей Скворцов <SVSkvortsov@1cbit.ru>
2020-03-10 13:29:12 +0300

    Name of commit

---
+++ Hello-Data/Hello-Data.js
@@ -0,0 +1,14 @@
+define( [],
+    function ( ) {
+        'use strict';
+
+        return {
+            definition: {},
+            initialProperties: {},
+            paint: function ( $element, layout ) {
+
+                // Your code comes here
+
+            }
+        };
+    } );
